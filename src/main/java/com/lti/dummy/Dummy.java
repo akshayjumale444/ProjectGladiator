1d9a8cab51b7972e1ede3941a664a84bb943acd5
Felix Biju <felix.biju@lntinfotech.com>
2019-08-13 15:42:35 -0700

    Commiting completed 'ADMIN MODULE'

--- src/main/java/com/lti/dummy/Dummy.java
+++ src/main/java/com/lti/dummy/Dummy.java
@@ -1,20 +1,23 @@
 package com.lti.dummy;
 
-import java.sql.Time;
-import java.util.Date;
+import java.time.LocalTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class Dummy {
-	private Date departure;
+	
+	@DateTimeFormat(pattern="hh:mm")
+	@JsonFormat(pattern="hh:mm")
+	private LocalTime departure;
 
-	public Date getDeparture() {
+	public LocalTime getDeparture() {
 		return departure;
 	}
 
-	public void setDeparture(Date departure) {
+	public void setDeparture(LocalTime departure) {
 		this.departure = departure;
 	}
-
-	
-	
 
 }
